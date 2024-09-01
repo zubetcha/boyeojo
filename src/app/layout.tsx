a8ecc9d59a15a758fdd9cf9c06cf35b071ce4a5d
JeongJuhye <juhjeong@lifegoeson.kr>
2024-09-01 23:39:55 +0900

    애드센스 스크립트 삽입

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,10 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "캐릭터 정보 보여조",
   description: "혈반 가호 잘 끼고 있나!",
+  viewport: {
+    width: "device-width", 
+    initialScale: 1
+  }
 };
 
 export default function RootLayout({
@@ -16,6 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9638489162996299"
+          crossOrigin="anonymous"></script>
+      </head>
       <body className={`${inter.className} h-full`}>{children}</body>
     </html>
   );
